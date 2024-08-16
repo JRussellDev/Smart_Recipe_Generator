44cb9df90306c4819441a3904039d38aa8eb1ffd
up2056252 <147083370+up2056252@users.noreply.github.com>
2024-08-16 12:52:23 +0100

    Feat: added more visual elements to the landingpage

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const arrowButton = document.getElementById('arrow-button');
+const darkSegment = document.getElementById('dark-segment');
+
+arrowButton.addEventListener('mouseover', () => {
+    darkSegment.style.boxShadow = '0 0 20px 10px #F5853F';
+});
+
+arrowButton.addEventListener('mouseout', () => {
+    darkSegment.style.boxShadow = 'none';
+});
+
+arrowButton.addEventListener("click", () => {
+    window.location.href = "generator.html";
+});
